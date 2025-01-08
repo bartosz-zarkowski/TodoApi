0059d7be577cb77220bf68820b6c67fef29559be
Bartosz Żarkowski <bartoszzarkowski@gmail.com>
2025-01-08 22:09:45 +0100

    Add sample integration tests for CategoryController

---
+++ TodoApi.Tests/Configs/CustomWebApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Configs;
+
+namespace TodoApi.Tests.Configs;
+
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    private readonly PostgresTestContainer _postgresContainer = new();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TodoDbContext>));
+            if (descriptor != null) services.Remove(descriptor);
+
+            services.AddDbContext<TodoDbContext>(options =>
+            {
+                options.UseNpgsql(_postgresContainer.ConnectionString);
+            });
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<TodoDbContext>();
+            db.Database.Migrate();
+        });
+    }
+
+    public async Task InitializeAsync() => await _postgresContainer.InitializeAsync();
+    public async Task DisposeAsync() => await _postgresContainer.DisposeAsync();
+}
---
+++ TodoApi.Tests/Configs/PostgresTestContainer.cs
@@ -0,0 +1,31 @@
+﻿
+using Testcontainers.PostgreSql;
+
+namespace TodoApi.Tests.Configs;
+
+public class PostgresTestContainer : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _container;
+
+    public string ConnectionString => _container.GetConnectionString();
+
+    public PostgresTestContainer()
+    {
+        _container = new PostgreSqlBuilder()
+            .WithDatabase("TestDb")
+            .WithUsername("postgres")
+            .WithPassword("password")
+            .WithCleanUp(true)
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _container.DisposeAsync();
+    }
+}
